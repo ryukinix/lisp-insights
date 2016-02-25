6d8bbe30c58c997e5a8d677ad719d30c16fc9db1
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-25 06:12:52 -0300

    Finish 4th chapter, data comparators and checkers functions
    member and find-if are really nice functions.
    Data comparison in lisp is a little messy, but using eq and equal
    rule appears be nice.

--- land-of-lisp/cap4-conditionals.lisp
+++ land-of-lisp/cap4-conditionals.lisp
@@ -95,9 +95,9 @@
 
 (nice-side-effect-lol 5) *number-was-odd*
 
-;; conditiions;
-;; if, when unless command
-;; case, cond or
+;;
+;; BASIC IDEAS ABOUT CONDITIONALS
+;;
 
 (defvar *number-is-odd* nil)
 (when (oddp 5)
@@ -134,6 +134,12 @@
 
 (nice-side-effect-lol 5)
 
+
+;;
+;; SYMMETRY IF-ELSE: WHEN AND UNLESS
+;;
+
+
 ;; conditiions;
 ;; if, when unless command
 ;; case, cond or
@@ -152,6 +158,11 @@
 (unless (oddp 4)
   (setf *number-is-odd* nil)
   'even-number) ;; -> even-number set *number-is-odd* = nil
+
+
+;;
+;; COND AND CASE
+;;
 
 ;; the command that does it all: cond
 ;; the cond form is the classic way
@@ -192,6 +203,9 @@
 ;; you choice the case <var> and compares later in the individual branchs
 ;; with it.
 
+;;
+;; AND-OR AS IF CONDITIONALS
+;;
 
 ;; now we think about the obscure use of conditionals using
 ;; only booleans expressions like 'and' and 'or'.
@@ -227,7 +241,8 @@
 
 (and *file-modified* (ask-user-about-saving) (save-file))
 
-;; The and evaluate sequencialy the expressions, but for that, (save-file) needs to returns a t value althoug
+;; The and evaluate sequencialy the expressions, but for that,
+;; (save-file) needs to returns a t value althoug
 ;; that kind of function don't explicit mean that, save-file may return other things.
 ;; We have a problem with that and some lispers can be say is not cool.
 ;; A third version of that, and a bit more clear, can be:
@@ -235,3 +250,104 @@
 (if (and *file-modified*
          (ask-user-about-saving))
     (save-file))
+
+;; using functions that return more than just truth value
+;; checking if something inside the list
+
+(if (member 1 '(4 3 2 1 5))
+    'one-is-in-the-list
+    'one-is-not-the-list) ;; -> one-is-in-the-list
+
+;;
+;; MEMBER AND FIND-IF
+;;
+
+;; nice, the behavior is correct, otherwhise... member have a little non-trivial returns.
+;; what you think member returns? t or nil? No. See:
+
+(member 1 '(4 3 2 1 5)) ; -> '(1 5)
+(member 2 '(4 3 2 1 5)) ; -> '(2 1 5)
+(member nil '(4 3 2 1 nil)) ; -> (nil)
+
+;; observes... (nil) != nil, '(nil) is a list with contem a nil atom, or empty list. Is like '(()) != '()
+
+;; Then 'member' returns nil if not found, right, but if true return more of just 't'.
+;; Return the value found until the tail. Whose really make senses if you remeber the way a list is constructed.
+;; '(4 3 2 1 nil) is equal to (cons 4 (cons 3 (cons 2 (cons 1 (cons nil nil)))))
+;; so if I found 2, is only need return the list itself whose will content the values until the tail.
+
+;; If you are asking some about "Why doesn't return the value it found, instead the tail?". Remember the means
+;; of t and nil. T is anything who doesn't is nil. So, check that example:
+
+(if (member nil '(1 2 3 4 nil))
+    'nil-inside-of-the-list
+    'nil-not-found)
+;; if member returns the value found, 'nil' so the if will be false and will return 'nil-not-found'
+;; whose don't make any sense. Instead that, (member nil '(1 2 3 4 nil))-> (nil)
+
+;; other functions whose can be beneficit of that kind of result is find-if
+;; (find-if #'lambda list)
+(find-if #'oddp '(0 2 3 4 5)) ;-> 3
+
+;; whose is something like any() function
+
+(if (find-if #'oddp '(0 1 2 3 4 5))
+    :we-have-an-odd-number
+    :no-odd-number-found)
+
+;; by other hand... if we searching about nil? HMM, think:
+
+(if (find-if #'null '(1 abacate nil something))
+    :we-found-a-nil-value
+    :no-nil-value-here) ; -> no-nil-value-here
+
+;; We have now at disapointment here, because find-if return the first value found
+;; whose is filtered by the function passed.
+;; Unfortunelly, we can't use find-if for if statement in that case.
+;; If find-if was equal to member function maybe will can be.
+;; These kind of small things that make even grown lispers shed had a tear
+
+
+;; COMPARING STUFF: eq, equal and More...
+
+;; We have a lot of comparison functions in common lisp
+;; in which is a kind of thing is not beauty on lisp.
+;; we have, eq, equal, string-equal, equalp, eql...
+
+;; Conrad'Rules say:
+;; -> use eq for compare symbols
+;; -> use equal for compare everything else
+
+;; eq: symbols
+(defparameter *fruit* 'apple)
+
+(cond ((eq *fruit* 'apple) 'its-an-apple)
+      (eq *fruit* 'orange) 'its-an-orange)
+
+;; equal: anything
+
+(equal 'banana 'banana) ;; symbols workin, but use eq ever (more fast)
+(equal '(1 2 3 4) '(1 2 3 4)) ;; arbitrary lists
+(equal '(3 2 1) (cons 3 (cons 2 (cons 1 nil)))) ;; lists constructed in different ways
+(equal 4 4) ;. comparison of integers
+(equal 4.5 4.5) ;; floats
+(equal "abacate" "abacate") ;; strings
+(equal #\a #\a) ;; chars
+
+
+;; eql: symbols, numbers and chars (don't use for strings)
+(eql "asdf" "asdf") ; -> nil!
+(eql :asdf :asdf) ; -> t
+(eql #\n #\n) ; -> t
+(eql 4.222225 4.222225) ;-> t
+
+;; equalp is like equal but using more abstract comparisons, like
+;; ignoring case for strings and float numbers comparisons
+(equalp "MAnoel" "manoel") ;-> t
+(equalp 1.0 1) ; -> t
+
+;; resume about I wrote in that file
+;; conditionals: if, when, unless, cond, case
+;; black-magic-conditionals: and, or
+;; checkers: member, find-if
+;; comparators: eq, eql, equal, equalp, string-equal, =
