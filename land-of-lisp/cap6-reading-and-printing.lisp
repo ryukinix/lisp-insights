0c850cac2fa79f7d9bdd9be7ba875aa53cebb6c9
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-29 09:32:17 -0300

    Add more things about reading and printing, update page count too

---
+++ land-of-lisp/cap6-reading-and-printing.lisp
@@ -0,0 +1,41 @@
+;; Common Lisp Script
+;; Manoel Vilela
+
+;; fundamentals printing
+
+;; :: print
+;; print send the arg to sdout with #\newline before the sentence 
+;; and add on the end a white-space too LOL something like that: 
+;; (format t "~% ~s " 'simbol) ;; realy crazy!!!!
+;; and return the output
+(print 'abacate)
+(print '(some data))
+(print ':this-is-a-keyword)
+(print "that is a string")
+
+;; :: prin1
+;; same thing of the 'print' function, but return a space
+;; instead newline
+(progn (prin1 "1")
+       (prin1 "2")
+       (prin1 "3"))
+
+;; simple example combining things
+;; remember: read sucks
+;; read operator only reads a single token. 
+;; if you type two tokens will break
+(defun say-hello ()
+  (print "Please type your name: ")
+  (let ((name (read)))
+    (prin1 "Nice to meet you, ")
+    (prin1 name)))
+(say-hello)
+
+;; but print and prin1 is repl stuff, don't cool for humans
+;; print strings with quotes and had a print esoteric behavior of newline-content-space
+;; instead use that, use princ! and read-line
+(defun say-hello ()
+  (princ "Please type your name: ")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
