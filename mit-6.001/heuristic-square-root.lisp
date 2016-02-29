7e997ffa23bdadae4a7ddd26421f2c93e2b66df5
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-29 04:41:40 -0300

    Grouping exclusive-use functions of square-root by black box technique
    The same is made for eval-test and call-test, because call-test is
    only used inside eval-test.
    
    That type of things avoid pollute the namespace with useless functions

--- mit-6.001/heuristic-square-root.lisp
+++ mit-6.001/heuristic-square-root.lisp
@@ -16,51 +16,46 @@
                         (square-root 165465)
                         ))
 
-(defun square (x)
-  (* x x))
+;; square-root black box
+;; functional
+(defun square-root (x) 
+  (labels ((square (x)
+              (* x x))
+           (average (x y) 
+             (/ (+ x y) 2))
+           (improve (guess x)
+             (average guess (/ x guess)))
+           (good-enough? (guess x)
+             (< (abs (- (square guess) x))
+                *error-limit*))
+           (try (guess x)
+             (if (good-enough? guess x)
+                 guess
+                 (try (improve guess x) x))))
+  
+  (float (try 1 x))))
 
 
-(defun average (x y) 
-  (/ (+ x y) 2))
-
-
-(defun improve (guess x)
-  (average guess (/ x guess)))
-
-
-(defun good-enough? (guess x)
-  (< (abs (- (square guess) x))
-     *error-limit*))
-
-
-(defun try (guess x)
-  (if (good-enough? guess x)
-      guess
-      (try (improve guess x) x)))
-
-
-(defun square-root (x) (float (try 1 x)))
-
-
-(defun call-test (test)
-  (let ((output (eval test)))
-       (format t "~s -> ~f ~%" test output)
-       output))
-
-
-(defun eval-test (test fn)
-   (let ((x (cadr test)))
-       (if (< (abs (- (call-test test) (funcall fn x)))
-              *error-limit*)
+;; eval-test black-box
+;; functional
+(defun eval-test (test fn limit)
+  (labels ((call-test (test)
+             (let ((output (eval test)))
+                   (format t "~s -> ~f ~%" test output)
+                   output)))
+    (let ((x (cadr test)))
+        (if (< (abs (- (call-test test) (funcall fn x)))
+              limit)
            :nice
-           :fail)))
+           :fail))))
 
-(defun run-tests (tests)
+;; non-functional use variable global *error-limit* and *tests*
+(defun run-tests ()
   (format t "Running tests with limit ~f ~%" *error-limit*)
-  (let* ((results (loop for x in *tests* collect (eval-test x #'sqrt)))
+  (let* ((results (loop for x in *tests* collect (eval-test x #'sqrt *error-limit*)))
          (total (length results))
          (pass (count :nice results)))
       (format t "Tests avalied [pass/total]: ~d/~d ~%" pass total)))
 
 
-(run-tests *tests*)
+(run-tests)
