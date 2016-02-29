26fd2f69bef3ed653ad95f6b227edebc5dde1376
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-29 07:00:51 -0300

    Add optional paramater next as lambda expressions for sigma-sum

--- mit-6.001/lambda-expressions.lisp
+++ mit-6.001/lambda-expressions.lisp
@@ -4,14 +4,14 @@
 
 ;; higher-order function sigma-sum
 ;; func must be a lambda(x) expression
-(defun sigma-sum (func a b)
+(defun sigma-sum (func a b &key (next #'1+))
   (if (> a b)
       0
       (+ (funcall func a)
-         (sigma-sum func (1+ a) b))))
+         (sigma-sum func (funcall next a) b))))
 
 (defun square (x)
   (* x x))
 
-(sigma-sum #'(lambda(x) (/ 1 x)) 1 10)
+(sigma-sum #'(lambda(x) (/ 1 x)) 1 10 :next #'(lambda (x) (+ 4 x)))
 (sigma-sum #'(lambda(x) (- x)) 1 10)
