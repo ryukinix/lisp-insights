35058fcb2be11f260d910ec7a2142c73ee629dd3
Manoel Vilela <manoel_vilela@engineer.com>
2016-03-01 07:27:39 -0300

    Finished the basic part of printing-reading of cap6
    Now the next lectures covers to extended the
    text game engine used at the earlier chapter 5 using
    the basic functions of i/o:
    
    prin1, print, princ, read
    read-line and eval.

--- land-of-lisp/cap6-reading-and-printing.lisp
+++ land-of-lisp/cap6-reading-and-printing.lisp
@@ -4,8 +4,8 @@
 ;; fundamentals printing
 
 ;; :: print
-;; print send the arg to sdout with #\newline before the sentence 
-;; and add on the end a white-space too LOL something like that: 
+;; print send the arg to sdout with #\newline before the sentence
+;; and add on the end a white-space too LOL something like that:
 ;; (format t "~% ~s " 'simbol) ;; realy crazy!!!!
 ;; and return the output
 (print 'abacate)
@@ -22,7 +22,7 @@
 
 ;; simple example combining things
 ;; remember: read sucks
-;; read operator only reads a single token. 
+;; read operator only reads a single token.
 ;; if you type two tokens will break
 (defun say-hello ()
   (print "Please type your name: ")
@@ -34,8 +34,20 @@
 ;; but print and prin1 is repl stuff, don't cool for humans
 ;; print strings with quotes and had a print esoteric behavior of newline-content-space
 ;; instead use that, use princ! and read-line
-(defun say-hello ()
+
+(progn (princ "An weird way to")
+       (princ #\newline)
+       (princ "To split a phrase"))
+
+(defun say-hello ()q
   (princ "Please type your name: ")
-  (let ((name (read-line)))
+  (let ((name (read-line))) ;; read-line is nice!
     (princ "Nice to meet you, ")
     (princ name)))
+
+;; other examples for princ
+(princ :foo)
+(princ 1.2)
+(princ "object")
+(princ 2/3)
+(princ 'symbol)
