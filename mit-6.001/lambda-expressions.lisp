aca9d49d357c03dd6aaba5fc82ebdd6987e8d5ea
Manoel Vilela <manoel_vilela@engineer.com>
2016-03-01 07:33:17 -0300

    Using cool sigma and lambda symbols at the code~

--- mit-6.001/lambda-expressions.lisp
+++ mit-6.001/lambda-expressions.lisp
@@ -1,30 +1,29 @@
 ;; Common Lisp Script
 ;; Manoel Vilela
 
-
-;; function sigma-sum
+;; function Σ
 ;; func must be a lambda(x) expression
-(defun sigma-sum (func a b &optional (next #'1+))
+(defun Σ (λ a b &optional (next #'1+))
   (if (> a b)
       0
-      (+ (funcall func a)
-         (sigma-sum func (funcall next a) b))))
+      (+ (funcall λ a)
+         (Σ λ (funcall next a) b))))
 
 ;; I would like to avoid the use of 'funcall' for calling
-;; lambda expressions passed to sigma-sum, is ugly.
+;; lambda expressions passed to Σ, is ugly.
 
 (defun sum-square (a b)
-  (sigma-sum #'(lambda(x) (* x x)) a b))
+  (Σ #'(lambda(x) (* x x)) a b))
 
 (defun sum-int (a b)
-  (sigma-sum #'(lambda(x) x) a b))
+  (Σ #'(lambda(x) x) a b))
 
 (defun sum-pi (a b)
-  (sigma-sum #'(lambda (x) (/ 1 (* x (+ 1 2)))) 
+  (Σ #'(lambda (x) (/ 1 (* x (+ 1 2)))) 
              a b
              #'(lambda (x) (+ x 4))))
 
 (sum-square 1 10)
 (sum-int 1 10)
-(sigma-sum #'(lambda(x) (/ 1 x)) 1 10 #'(lambda (x) (+ 4 x)))
-(sigma-sum #'(lambda(x) (- x)) 1 10)
+(Σ #'(lambda(x) (/ 1 x)) 1 10 #'(lambda (x) (+ 4 x)))
+(Σ #'(lambda(x) (- x)) 1 10)
