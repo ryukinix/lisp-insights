259299c5f313bfa5c2c61f6da9a8264b719c2a74
Manoel Vilela <manoel_vilela@engineer.com>
2017-01-28 15:41:50 -0300

    Use emacs to indent game-of-life-sdl.lisp correctly

--- random/game-of-life-sdl.lisp
+++ random/game-of-life-sdl.lisp
@@ -16,9 +16,9 @@
 
 (defun count-neighboring-individual (i j world)
   (let ((next-i (if (= i (1- (array-dimension world 0))) 0 (1+ i)))
-    (prev-i (if (= i 0) (1- (array-dimension world 0)) (1- i)))
-    (next-j (if (= j (1- (array-dimension world 1))) 0 (1+ j)))
-    (prev-j (if (= j 0) (1- (array-dimension world 1)) (1- j))))
+        (prev-i (if (= i 0) (1- (array-dimension world 0)) (1- i)))
+        (next-j (if (= j (1- (array-dimension world 1))) 0 (1+ j)))
+        (prev-j (if (= j 0) (1- (array-dimension world 1)) (1- j))))
     (+ (aref world prev-i prev-j)
        (aref world prev-i j)
        (aref world prev-i next-j)
@@ -33,31 +33,31 @@
   (let ((next-world (alexandria:copy-array  world)))
     (loop for i from 0 to (1- (array-dimension world 0)) do
       (loop for j from 0 to (1- (array-dimension world 1)) do
-    (cond ((and (zerop (aref world i j)) ; birth
-            (= (count-neighboring-individual i j world) 3))
-           (setf (aref next-world i j) 1))
-          ((and (= (aref world i j) 1)   ; die by under-population or overcrowding
-            (or (<= (count-neighboring-individual i j world) 1)
-            (>= (count-neighboring-individual i j world) 4)))
-           (setf (aref next-world i j) 0)))))
+        (cond ((and (zerop (aref world i j)) ; birth
+                    (= (count-neighboring-individual i j world) 3))
+               (setf (aref next-world i j) 1))
+              ((and (= (aref world i j) 1)   ; die by under-population or overcrowding
+                    (or (<= (count-neighboring-individual i j world) 1)
+                        (>= (count-neighboring-individual i j world) 4)))
+               (setf (aref next-world i j) 0)))))
     next-world))
 
 (defun life ()
   (sdl:with-init ()
     (sdl:window 400 400) ; size of window
-    (setf (sdl:frame-rate) 30) ; set frame-rate 60fps
+    (setf (sdl:frame-rate) 60) ; set frame-rate 60fps
     (init-world! *world*)
     (sdl:with-events ()
       (:quit-event () t)
       (:idle ()
-         (setf *world* (update-next-generation *world*))
-         (loop for i from 0 to (1- (array-dimension *world* 0)) do
-           (loop for j from 0 to (1- (array-dimension *world* 1)) do
-         (if (= (aref *world* i j) 0)
-             (sdl-gfx:draw-box (sdl:rectangle :x (* i 4) :y (* j 4) :w 4 :h 4)
+             (setf *world* (update-next-generation *world*))
+             (loop for i from 0 to (1- (array-dimension *world* 0)) do
+               (loop for j from 0 to (1- (array-dimension *world* 1)) do
+                 (if (= (aref *world* i j) 0)
+                     (sdl-gfx:draw-box (sdl:rectangle :x (* i 4) :y (* j 4) :w 4 :h 4)
                                        :color sdl:*black*)
-             (sdl-gfx:draw-box (sdl:rectangle :x (* i 4) :y (* j 4) :w 4 :h 4)
+                     (sdl-gfx:draw-box (sdl:rectangle :x (* i 4) :y (* j 4) :w 4 :h 4)
                                        :color sdl:*white*))))
-         (sdl:update-display)))))
+             (sdl:update-display)))))
 
 (life)
