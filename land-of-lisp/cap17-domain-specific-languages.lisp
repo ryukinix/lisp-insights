873f529d5ac9b7fb28a9cd16f43809e48e7c29df
Manoel Vilela <manoel_vilela@engineer.com>
2017-03-16 21:15:05 -0300

    Split Chapter 17 file: (svg-dsl, new-text-game-adventure)
    This keep the things more simple because the new text game adventure
    need executes your runtime at CL-USER package, but, in another hand, :svg
    is a package isolated.

--- land-of-lisp/cap17-domain-specific-languages.lisp
+++ land-of-lisp/cap17-domain-specific-languages.lisp
@@ -5,7 +5,15 @@
 ;; Remember SVG (Scalable Vector Graphics) are XML-like,
 
 
-(load "cap5-building-a-text-game-engine")
+(defpackage :svg
+  (:use :cl)
+  (:export :svg :html
+           :body :tag
+           :brightness :svg-style
+           :polygon :circle))
+
+(in-package :svg)
+
 
 ;; Would be better avoid copy-paste functions,
 ;; but the chapter 16 has several re-definitions
@@ -128,53 +136,3 @@
 ;; svg finished
 
 ;; starts extension of the wizard_game from chapter 5-6
-
-(defun have (object)
-  (member object (inventory)))
-
-(defparameter *chain-welded* nil)
-
-(defun weld (subject object)
-  (if (and (eq *location* 'attic)
-           (eq subject 'chain)
-           (eq object 'bucket)
-           (have 'chain)
-           (have 'bucket)
-           (not *chain-welded*))
-      (progn (setf *chain-welded* t)
-             '(the chain is now securely welded to the bucket.))
-      '(you cannot weld like that.)))
-
-(pushnew 'weld *allowed-commands*)
-(defparameter *bucket-filled* nil)
-
-(defun dunk (subject object)
-  (if (and (eq *location* 'garden)
-           (eq subject 'bucket)
-           (eq object 'well)
-           (have 'bucket)
-           *chain-welded*)
-      (progn (setf *bucket-filled* t)
-             '(the bucket is now full of water))
-      '(you cannot dunk like that)))
-(pushnew 'dunk *allowed-commands*)
-
-
-;; super cool macro to avoid replication like the commands above
-(defmacro game-action (command subj obj place &body body)
-  `(progn (defun ,command (subject object)
-            (if (and (eq *location* ',place)
-                     (eq subject ',subj)
-                     (eq object ',obj)
-                     (have ',subj))
-                ,@body
-                '(i cant ,command like that.)))
-          (pushnew ',command *allowed-commands*)))
-
-(game-action splash bucket wizard living-room
-  (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
-        ((have 'frog) '(the wizard awakens and sees that you stole his frog.
-                        he is so upset he banishes you to the netherworlds-
-                        you lose! the end.))
-        (t '(the wizard awakens from his slumber and greets your warmly.
-             he hands you the magic low-carb donut- you win! the end.))))
